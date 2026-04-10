a4423dcb177e6c839eb4229429f6cc3423ee3845
fourthdm <manisha.fourthdm@gmail.com>
2026-04-10 18:04:24 +0530

    update in service component

--- src/app/pages/service/service.component.ts
+++ src/app/pages/service/service.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-service',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./service.component.css']
 })
 export class ServiceComponent {
+
+  sendEmail(event: Event) {
+    event.preventDefault();
+
+    emailjs.sendForm(
+      'service_7ugy8vj',       // e.g., service_xxxxxx
+      'template_p367rob',      // e.g., template_yyyyyy
+      event.target as HTMLFormElement,
+      '_iV-VaDzP7_2BoTII'        // e.g., public_zzzzzz
+    ).then(
+      () => {
+        alert('Enquiry sent successfully!');
+      },
+      (error) => {
+        console.error('FAILED...', error);
+        alert('Failed to send enquiry.');
+      }
+    );
+
+    (event.target as HTMLFormElement).reset();
+  }
 
 }
